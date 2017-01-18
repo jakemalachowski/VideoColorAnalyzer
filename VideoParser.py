e32752da43813bacab6266bb9f1479b799719b82
Jacob <jmalachowski623@gmail.com>
2017-01-17 21:36:04 -0600

    Added ability to call from command line and select filename

--- VideoParser.py
+++ VideoParser.py
@@ -1,16 +1,26 @@
+import getopt
+
 from PIL import Image
 import subprocess as sp
 import numpy
 from matplotlib import pyplot
+from pathlib import Path
+import argparse
+
+
+def usage():
+    print("USAGE: videoparser.py {FILENAME} -f --FRAME {NUMBER OF FRAMES TO SKIP")
 
 
 def getaveragecolor(file):
     im = Image.open(file)
 
-    size = (1000, 1000)
+    size = (100, 100)
 
     # create list of pixel's RGB values and their count (stored in color[0])
     colors = im.getcolors(1000000)
+
+    im.close()
 
     count = 0
     c1 = 0
@@ -30,28 +40,48 @@ def getaveragecolor(file):
     c2 /= count
     c3 /= count
 
-    print(c1, c2, c3)
     return Image.new("RGB", size, (int(c1), int(c2), int(c3)))
 
 
 FFMPEG_BIN = "ffmpeg.exe"
 
+parser = argparse.ArgumentParser(description="Analyze the change in colors of videos over time")
+parser.add_argument('-f', "--frames", default=24)
+parser.add_argument('--file', dest="FILENAME", required=True )
+args = vars(parser.parse_args())
+
+print(args)
+
+FILENAME = args['FILENAME']
+if args['frames']:
+    FRAME_SKIP_COUNT = int(args['frames'])
+print("Frame skip count", FRAME_SKIP_COUNT)
+
+
+my_file = Path(FILENAME)
+if not my_file.is_file():
+    raise FileNotFoundError('File was not found', args[0])
+
 command = [FFMPEG_BIN,
-           '-i', "Im_an_Albatraoz.mp4",
+           '-i', FILENAME,
+          # '-ss', '00:02:22',
            '-f', 'image2pipe',
            '-pix_fmt', 'rgb24',
            '-vcodec', 'rawvideo', '-']
 
 pipe = sp.Popen(command, stdout=sp.PIPE, bufsize=10 ** 8, shell=True)
+print(FRAME_SKIP_COUNT)
 i = 0
 while True:
 
     # read 1 frame
     raw_image = pipe.stdout.read(1280*720*3)
 
-    if i % 24 == 0:
+    if i % FRAME_SKIP_COUNT == 0:
         # transform the byte read into a numpy array
         image = numpy.fromstring(raw_image, dtype='uint8')
+        if image.size == 0:
+            break
         image = image.reshape((720, 1280, 3))
         # Setup details to trim the borders of the PyPlot figure
         fig = pyplot.figure()
@@ -64,6 +94,7 @@ while True:
 
         # Save file without borders
         pyplot.savefig("img" + str(i) + ".png", bbox_inches=extent)
+        pyplot.close()
 
         # Get average color of the last saved image
         avgImage = getaveragecolor("img" + str(i) + ".png")
@@ -71,4 +102,3 @@ while True:
     # throw away the data in the pipe's buffer.
     pipe.stdout.flush()
     i += 1
-
