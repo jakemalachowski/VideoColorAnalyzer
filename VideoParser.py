06db81bc3d96b81fa6bc7e9b47ea81400c187a13
Jacob <jmalachowski623@gmail.com>
2017-01-19 13:42:47 -0600

    Added drawing functionality

--- VideoParser.py
+++ VideoParser.py
@@ -1,52 +1,15 @@
-import getopt
-
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageColor
 import subprocess as sp
 import numpy
 from matplotlib import pyplot
 from pathlib import Path
 import argparse
+import AverageColor
 
 
 def usage():
     print("USAGE: videoparser.py {FILENAME} -f --FRAME {NUMBER OF FRAMES TO SKIP")
 
-
-def clamp(x):
-  return max(0, min(x, 255))
-
-
-def getaveragecolor(file):
-    im = Image.open(file)
-
-    size = (100, 100)
-
-    # create list of pixel's RGB values and their count (stored in color[0])
-    colors = im.getcolors(1000000)
-
-    im.close()
-
-    count = 0
-    c1 = 0
-    c2 = 0
-    c3 = 0
-
-    # add all the values up and divide by the total number of
-    # pixels to get the average rgb value
-
-    for color in colors:
-        count += color[0]
-        c1 += (color[1][0] * color[0])
-        c2 += (color[1][1] * color[0])
-        c3 += (color[1][2] * color[0])
-
-    c1 /= count
-    c2 /= count
-    c3 /= count
-
-    return (int(c1), int(c2), int(c3))
-    # return hex color
-    # return "#{0:02x}{1:02x}{2:02x}".format(clamp(int(c1)), clamp(int(c2)), clamp(int(c3)))
 
 FFMPEG_BIN = "ffmpeg.exe"
 
@@ -65,7 +28,7 @@ print("Frame skip count", FRAME_SKIP_COUNT)
 
 my_file = Path(FILENAME)
 if not my_file.is_file():
-    raise FileNotFoundError('File was not found', args[0])
+    raise FileNotFoundError('File was not found', args['FILENAME'])
 
 command = [FFMPEG_BIN,
            '-i', FILENAME,
@@ -75,10 +38,10 @@ command = [FFMPEG_BIN,
            '-vcodec', 'rawvideo', '-']
 
 pipe = sp.Popen(command, stdout=sp.PIPE, bufsize=10 ** 8, shell=True)
-print(FRAME_SKIP_COUNT)
 i = 0
-finalImage = Image.new("RGB", (1000, 20000))
-finalImageDraw = ImageDraw.Draw(finalImage)
+pos = 0
+finalImage = Image.new("RGB", (20000, 1000))
+finalImageDraw = ImageDraw.Draw(finalImage, "RGB")
 
 while True:
     # read 1 frame
@@ -101,14 +64,17 @@ while True:
         extent = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
 
         # Save file without borders
-        pyplot.savefig("img" + str(i) + ".png", bbox_inches=extent)
+        pyplot.savefig("frame" + str(i) + ".png", bbox_inches=extent)
         pyplot.close()
 
         # Get average color of the last saved image
-        avgColor = getaveragecolor("img" + str(i) + ".png")
-        print(avgColor)
-        finalImageDraw.line([(i, 0), (i, 1000)], fill=avgColor)
+        avgColor = AverageColor.averagecolorfromfile("frame" + str(i) + ".png")
+        color = ImageColor
+        finalImageDraw.line([(pos, 0), (pos, 1000)], fill="rgb" + str(avgColor))
+        pos += 1
     # throw away the data in the pipe's buffer.
     pipe.stdout.flush()
     i += 1
-finalImage.save("final_image.png")
+
+finalImage = finalImage.crop([0, 0, pos, 1000])
+finalImage.save(FILENAME + " - color.png")
