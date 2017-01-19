1cd92f26db7f67ddd68656c70528ae6c2d1d1409
Jacob <jmalachowski623@gmail.com>
2017-01-19 15:09:11 -0600

    Added the documentation

--- VideoParser.py
+++ VideoParser.py
@@ -9,37 +9,44 @@ import AverageColor
 def usage():
     print("USAGE: videoparser.py {FILENAME} -f --FRAME {NUMBER OF FRAMES TO SKIP")
 
-
-FFMPEG_BIN = "ffmpeg.exe"
-
+# Get the arguments from the command line and assign them to variables
 parser = argparse.ArgumentParser(description="Analyze the change in colors of videos over time")
 parser.add_argument('-f', "--frames", dest="FRAMESKIPCOUNT", default=24)
 parser.add_argument('--file', dest="FILENAME", required=True)
 args = vars(parser.parse_args())
 
-print(args)
 FRAME_SKIP_COUNT = args['FRAMESKIPCOUNT']
 FILENAME = args['FILENAME']
 if args['FRAMESKIPCOUNT']:
     FRAME_SKIP_COUNT = int(args['FRAMESKIPCOUNT'])
 print("Frame skip count", FRAME_SKIP_COUNT)
 
-
+# Make sure the file is valid
 my_file = Path(FILENAME)
 if not my_file.is_file():
     raise FileNotFoundError('File was not found', args['FILENAME'])
 
+# Run the command to start the FFMPEG library
+FFMPEG_BIN = "ffmpeg.exe"
+
 command = [FFMPEG_BIN,
            '-i', FILENAME,
-           # '-ss', '00:02:00',
            '-f', 'image2pipe',
            '-pix_fmt', 'rgb24',
            '-vcodec', 'rawvideo', '-']
 
+# Open pipe to start receiving pixel data
 pipe = sp.Popen(command, stdout=sp.PIPE, bufsize=10 ** 8, shell=True)
+# i is for processing every 'FRAME_SKIP_COUNT'th element
 i = 0
+# pos is to hold the position to draw the next line
 pos = 0
-finalImage = Image.new("RGB", (20000, 1000))
+
+# TODO: Calculate the number of frames to base the height and width
+width = 20000
+height = 1000
+# Declare a blank image and prepare it for drawing
+finalImage = Image.new("RGB", (20000, height))
 finalImageDraw = ImageDraw.Draw(finalImage, "RGB")
 
 while True:
@@ -50,17 +57,20 @@ while True:
         # transform the byte read into a numpy array
         image = numpy.fromstring(raw_image, dtype='uint8')
         if image.size == 0:
-            # Reached the end of the video
+            # No more data, reached the end of the video
             break
+        # Put the data into an image for processing (Can this be skipped to improve performance?)
         image = image.reshape((720, 1280, 3))
         img = Image.fromarray(image, 'RGB')
 
         avgColor = AverageColor.averagecolorfromimage(img)
-        finalImageDraw.line([(pos, 0), (pos, 1000)], fill="rgb" + str(avgColor))
+        # Draw a line
+        finalImageDraw.line([(pos, 0), (pos, height)], fill="rgb" + str(avgColor))
         pos += 1
     # throw away the data in the pipe's buffer.
     pipe.stdout.flush()
     i += 1
 
+# Crop the image and save it
 finalImage = finalImage.crop([0, 0, pos, 1000])
 finalImage.save(FILENAME + " - color.png")
