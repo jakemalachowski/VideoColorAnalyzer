9f4c3ffb4c3f17ac7f6dffd5299c3392b3574cd9
Sam Rose <samwho@lbak.co.uk>
2017-01-21 19:49:59 +0000

    Added some comments about tuning and added code to skip frames we haven't got the space to count colours for.

--- videoparser/frame.py
+++ videoparser/frame.py
@@ -2,6 +2,14 @@ import logging
 import numpy
 from PIL import Image
 
+# If you want to play around with the constants below, you can use the following
+# command to compare your output (provided you have imagemagick installed):
+#
+#   $ compare -verbose -metric fuzz img1.png img2.png diff.png
+#
+# The values below seem to be a small trade off in accuracy for a large trade
+# off in speed.
+
 
 # To save processing time, you can choose to downsize each frame before
 # calculating the average color. A larger scale factor results in faster
@@ -11,12 +19,12 @@ SCALE_FACTOR = 2 ** 4
 # While filter to use when resizing images. The fastest is Image.NEAREST, but
 # it's also the least accurate. Image.ANTIALIAS is slowest, but will result in a
 # better average. Image.BILINEAR and Image.BICUBIC are good tradeoffs.
-FILTER = Image.ANTIALIAS
+FILTER = Image.BICUBIC
 
 # The maximum number of colors you expect per frame. Because we have to allocate
 # the memory for these ahead of time, high numbers can be costly. However, if a
 # frame has more colors than this number in it, that frame is skipped.
-MAX_COLORS = 2 ** 10
+MAX_COLORS = 2 ** 14
 
 
 class Frame(object):
@@ -38,8 +46,11 @@ class Frame(object):
         colors = resized.getcolors(MAX_COLORS)
 
         if colors == None:
-            # TODO
-            pass
+            logging.warn(("frame %d skipped due to having more colors than we " +
+                "allocated space to count") % self.number)
+
+            # This results in a black line in the output image.
+            return (0, 0, 0)
 
         count = 0
         c1 = 0
