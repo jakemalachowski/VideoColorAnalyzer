cad550e0665c2dde100cba274626210dcca8f586
Sam Rose <samwho@lbak.co.uk>
2017-01-21 18:00:58 +0000

    Object orientation ftw

--- VideoParser.py
+++ VideoParser.py
@@ -1,6 +1,7 @@
 import logging
 import numpy
 import os
+import progressbar
 import sys
 import subprocess
 import time
@@ -30,85 +31,126 @@ logging.basicConfig(format="[%(asctime)s %(name)s %(levelname)s] %(message)s")
 logger = logging.getLogger("videoparser")
 logger.setLevel(logging.INFO)
 
-def get_resolution(path):
-    out = subprocess.check_output([FFPROBE_BIN, "-v", "error", "-of", "flat=s=_",
-        "-select_streams", "v:0", "-show_entries", "stream=height,width", path])
+class Video(object):
+    def __init__(self, path):
+        self.path = path
 
-    lines = out.split("\n")
-    width = lines[0].split('=')[1]
-    height = lines[1].split('=')[1]
-    return (int(width), int(height))
+        logger.info("determining video resolution...")
+        self.width, self.height = self._resolution(self.path)
+        logger.info("video resolution is: %dx%d" % (self.width, self.height))
 
-def get_num_frames(path):
-    out = subprocess.check_output([FFPROBE_BIN, "-count_frames",
-        "-select_streams", "v:0", "-show_entries", "stream=nb_read_frames",
-        "-of", "default=nokey=1:noprint_wrappers=1", "-v", "error", path])
-    return int(out)
+        self.bytes_per_frame = self.width * self.height * 3
 
-def get_video_stream(path, bufsize):
-    command = [FFMPEG_BIN, '-i', path, '-f', 'image2pipe',
-            '-pix_fmt', 'rgb24', '-vcodec', 'rawvideo', '-']
+        logger.info("counting frames...")
+        self.total_frames = self._total_frames(self.path)
+        logger.info("total frames in video: %d" % self.total_frames)
 
-    return subprocess.Popen(command, stdout=subprocess.PIPE, stderr=FNULL,
-            bufsize=bufsize)
+        logger.info("determining framerate...")
+        self.frame_rate = self._frame_rate(self.path)
+        logger.info("frame rate: %.2f" % self.frame_rate)
 
-def get_frame_rate(path):
-    out = subprocess.check_output([FFPROBE_BIN, "-v", "0", "-of", "compact=p=0",
-        "-select_streams", "0", "-show_entries", "stream=r_frame_rate", path])
-    n, d = out.split("=")[1].split("/")
-    return float(n) / float(d)
+        logger.info("opening video stream...")
+        self.proc = self._spawn(self.path)
+        logger.info("video stream opened")
 
-def get_average_color(raw_frame, width, height):
-    frame = numpy.fromstring(raw_frame, dtype='uint8')
-    frame = frame.reshape((height, width, 3))
-    image = Image.fromarray(frame, 'RGB')
+        self.current_frame = 0
 
-    width, height = image.size
+    def next_frame(self):
+        self.current_frame += 1
+        data = self.proc.stdout.read(self.bytes_per_frame)
 
-    image = image.resize((width / SCALE_FACTOR, height / SCALE_FACTOR), FILTER)
+        if len(data) != self.bytes_per_frame:
+            return None
 
-    # create list of pixel's RGB values and their count (stored in color[0])
-    colors = image.getcolors(MAX_COLORS)
+        return Frame(data, self.width, self.height, self.current_frame)
 
-    count = 0
-    c1 = 0
-    c2 = 0
-    c3 = 0
+    def has_next_frame(self):
+        return self.current_frame <= self.total_frames
 
-    # add all the values up and divide by the total number of
-    # pixels to get the average rgb value
+    def _resolution(self, path):
+        out = subprocess.check_output([FFPROBE_BIN, "-v", "error", "-of",
+            "flat=s=_", "-select_streams", "v:0", "-show_entries",
+            "stream=height,width", path])
 
-    for color in colors:
-        count += color[0]
-        c1 += (color[1][0] * color[0])
-        c2 += (color[1][1] * color[0])
-        c3 += (color[1][2] * color[0])
+        lines = out.split("\n")
+        width = lines[0].split('=')[1]
+        height = lines[1].split('=')[1]
+        return (int(width), int(height))
 
-    c1 /= count
-    c2 /= count
-    c3 /= count
+    def _total_frames(self, path):
+        out = subprocess.check_output([FFPROBE_BIN, "-count_frames",
+            "-select_streams", "v:0", "-show_entries", "stream=nb_read_frames",
+            "-of", "default=nokey=1:noprint_wrappers=1", "-v", "error", path])
+        return int(out)
 
-    return int(c1), int(c2), int(c3)
+    def _frame_rate(self, path):
+        out = subprocess.check_output([FFPROBE_BIN, "-v", "0", "-of",
+            "compact=p=0", "-select_streams", "0", "-show_entries",
+            "stream=r_frame_rate", path])
+        n, d = out.split("=")[1].split("/")
+        return float(n) / float(d)
+
+    def _spawn(self, path):
+        command = [FFMPEG_BIN, '-i', path, '-f', 'image2pipe', '-pix_fmt',
+                'rgb24', '-vcodec', 'rawvideo', '-']
+
+        return subprocess.Popen(command, stdout=subprocess.PIPE, stderr=FNULL,
+                bufsize=self.bytes_per_frame * 300)
+
+
+class Frame(object):
+    def __init__(self, data, width, height, number):
+        self.data = data
+        self.width = width
+        self.height = height
+        self.number = number
+
+        self.np = numpy.fromstring(self.data, dtype='uint8')
+        self.np = self.np.reshape((self.height, self.width, 3))
+        self.image = Image.fromarray(self.np, 'RGB')
+
+    def average_color(self):
+        resized = self.image.resize((self.width / SCALE_FACTOR, self.height /
+            SCALE_FACTOR), FILTER)
+
+        # create list of pixel's RGB values and their count (stored in color[0])
+        colors = resized.getcolors(MAX_COLORS)
+
+        if colors == None:
+            # TODO
+            pass
+
+        count = 0
+        c1 = 0
+        c2 = 0
+        c3 = 0
+
+        # add all the values up and divide by the total number of
+        # pixels to get the average rgb value
+
+        for color in colors:
+            count += color[0]
+            c1 += (color[1][0] * color[0])
+            c2 += (color[1][1] * color[0])
+            c3 += (color[1][2] * color[0])
+
+        c1 /= count
+        c2 /= count
+        c3 /= count
+
+        return int(c1), int(c2), int(c3)
+
 
 def usage():
-    print("usage: videoparser.py video_file")
+    print("usage: videoparser.py video_file output.png")
 
-if len(sys.argv) != 2:
+
+if len(sys.argv) != 3:
     usage()
     sys.exit(1)
 
-path = sys.argv[1]
-width, height = get_resolution(path)
-logger.info("video resolution: %dx%d" % (width, height))
-
-bytes_per_frame = width * height * 3
-
-frame_rate = get_frame_rate(path)
-logger.info("video frame rate: %f" % frame_rate)
-
-logger.info("counting frames...")
-frames = get_num_frames(path)
-logger.info("total frames in video: %d" % frames)
+video = Video(sys.argv[1])
+out_path = sys.argv[2]
 
 out_width = 1920
 out_height = 1080
@@ -116,47 +158,30 @@ out_frame = 0
 
 # Number of frames to skip to ensure we only sample $out_width frames, so that
 # we create the ideal image and don't need to resize it later.
-frame_skip = int(frames / float(out_width))
+frame_skip = int(video.total_frames / float(out_width))
 
 # Declare a blank image and prepare it for drawing
 out_image = Image.new("RGB", (out_width, out_height))
 out_image_draw = ImageDraw.Draw(out_image, "RGB")
 
-stream = get_video_stream(path, 360 * bytes_per_frame)
-current_frame = 0
-last_frame = 0
-t = time.time()
+last_frame = None
 
-while True:
-    current_frame += 1
-    raw_frame = stream.stdout.read(bytes_per_frame)
+with progressbar.ProgressBar(max_value=video.total_frames) as bar:
+    while video.has_next_frame():
+        frame = video.next_frame()
 
-    if len(raw_frame) == 0:
-        logger.info("reached end of video file")
-        break
+        # Skip frames according to the given sample rate.
+        if frame.number % frame_skip != 0:
+            continue
 
-    # Skip frames according to the given sample rate.
-    if current_frame % frame_skip != 0:
-        continue
+        out_image_draw.line([(out_frame, 0), (out_frame, out_height)], fill="rgb" +
+                str(frame.average_color()))
+        out_frame += 1
 
-    avg_color = get_average_color(raw_frame, width, height)
+        if last_frame == None:
+            last_frame = frame
+            continue
 
-    out_image_draw.line([(out_frame, 0), (out_frame, out_height)],
-            fill="rgb" + str(avg_color))
-    out_frame += 1
+        bar.update(frame.number)
 
-    now = time.time()
-    time_delta = now - t
-    frame_delta = current_frame - last_frame
-    fps = frame_delta / time_delta
-    percent = current_frame / float(frames) * 100
-
-    # Counting frames per second like this is sort-of a lie because we're not
-    # processing every frame, but I've used it as a measure for how much
-    # improvement each change makes.
-    logger.info("[%.2f%%] processing %.2f fps" % (percent, fps))
-
-    last_frame = current_frame
-    t = now
-
-out_image.save(path + ".colors.png")
+out_image.save(out_path)
